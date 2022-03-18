52fc976b7e6acd0949da4ee1c83e53ff8e11bd53
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-18 12:45:32 +0530

    Update ShortDataType.java

--- DataTypes/ShortDataType.java
+++ DataTypes/ShortDataType.java
@@ -2,15 +2,15 @@ package com.tns.coreJava;
 
 public class ShortDataType 
 {
-	//range ---> -32767  to 32768
+  //range ---> -32767  to 32768
 	
-	    public static void main(String[] args) 
-	      {
-		       short num =32767;
+     public static void main(String[] args) 
+       {
+	  short num =32767;
            
-		       // short num= 32769;         //Error
-           // short num= -32769;        //Error
+          // short num= 32769;         //Error
+          // short num= -32769;        //Error
            
-		       System.out.println(num);
+          System.out.println(num);
         }
 }
