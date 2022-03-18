bb32797cca21d0be7e8367ccf5911d7cb3dced48
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-18 12:48:11 +0530

    Create ShortDataType.java

--- DataTypes/ShortDataType.java
+++ DataTypes/ShortDataType.java
@@ -4,7 +4,7 @@ public class ShortDataType
 {
   //range ---> -32767  to 32768
 	
-     public static void main(String[] args) 
+    public static void main(String[] args) 
        {
 	  short num =32767;
            
