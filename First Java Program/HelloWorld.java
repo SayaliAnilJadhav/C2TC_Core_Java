8920fa6259646592357e46df4f55ce95e8360bc9
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-18 12:08:16 +0530

    Update HelloWorld.java

--- First Java Program/HelloWorld.java
+++ First Java Program/HelloWorld.java
@@ -2,12 +2,10 @@ package com.tns.coreJava;
 
 public class HelloWorld 
 {
-
-	public static void main(String[] args) 
+   public static void main(String[] args) 
 	{
-		System.out.println("Hello World");
+	  System.out.println("Hello World");
 	}
-
 }
 
 
