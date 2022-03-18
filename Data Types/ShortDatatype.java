491409af26cd1d4eea9a7c099864e6fff605ddf2
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-18 11:42:10 +0530

    Add files via upload

---
+++ Data Types/ShortDatatype.java
@@ -0,0 +1,21 @@
+package com.tns.coreJava;
+
+public class ShortDatatype
+{
+   //range ---> -32767 to 32768
+	     public static void main(String[] args) 
+	        {
+	           short num =32767;
+	           
+	          // short num= 32769;         //Error
+	          // short num= -32769;        //Error
+	           
+	           System.out.println(num);
+	          
+	           
+	           
+		
+
+	        }
+
+}
