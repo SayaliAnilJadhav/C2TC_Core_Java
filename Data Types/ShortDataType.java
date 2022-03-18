fc73ea2d7b907762357fa072c28586f6cf67de89
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-18 11:48:36 +0530

    Create ShortDataType.java

---
+++ Data Types/ShortDataType.java
@@ -0,0 +1,17 @@
+package com.tns.coreJava;
+
+public class ShortDataType 
+{
+	//range ---> -32767  to 32768
+	    public static void main(String[] args) 
+	      {
+		    short num =32767;
+           
+		    // short num= 32769;         //Error
+            // short num= -32769;        //Error
+           
+		    System.out.println(num);
+            	 
+          }
+
+}
