02273ded436f3afce5c17d9ac490c1b1600c24b8
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-18 12:55:21 +0530

    Create IntDataType.java

--- DataTypes/IntDataType.java
+++ DataTypes/IntDataType.java
@@ -4,19 +4,21 @@ public class IntDataType
 {
    //range ---> -2147483648 to 2147483647
 	 
-	   public static void main(String[] args) 
-	    {
-		    int num = 10354;
-		    System.out.println(num);
+	public static void main(String[] args) 
+	  {
+             int num = 10354;
+	     System.out.println(num);
 		 
-		    //int num1 = 2147483648;      //Error
+	     //int num1 = 2147483648;      //Error
+             //System.out.println(num1);
 		  
-		    int num1 = 2147483647;
-		    System.out.println(num1);
+	     int num1 = 2147483647;
+	     System.out.println(num1);
 		  
-		    int num2 = -2147483648;
-		    System.out.println(num2);
+             int num2 = -2147483648;
+	     System.out.println(num2);
 		  
-		    //int num3 = -2147483649;    //Error
-		 }
+             //int num3 = -2147483649;    //Error
+	     System.out.println(num1);
+	 }
 }
