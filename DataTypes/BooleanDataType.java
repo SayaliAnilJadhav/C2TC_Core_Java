2167e3b78d884d0f73b59daed9347ca50b969c0c
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-22 11:33:31 +0530

    BooleanDataType.java

---
+++ DataTypes/BooleanDataType.java
@@ -0,0 +1,17 @@
+package com.tns.coreJava;
+
+public class BooleanDataType 
+{
+ //values ---> true and false
+	  public static void main(String[] args) 
+	    {
+		    boolean x = true;
+		    //boolean x = '1';       //Error
+		    //boolean x = 1;         //Error
+		    System.out.println(x);
+		
+		    int m = 100, n = 200;
+		    boolean b = m > n;
+		    System.out.println(b);
+	    }
+}
