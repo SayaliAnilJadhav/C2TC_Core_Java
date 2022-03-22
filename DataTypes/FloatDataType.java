21aaeeaaa41ffe3a8f504a86f6c5755e3592f4a1
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-22 11:42:47 +0530

    FloatDataType.java

--- DataTypes/FloatDataType.java
+++ DataTypes/FloatDataType.java
@@ -4,14 +4,15 @@ public class FloatDataType
 {
  //range --> 5 to 6 number precision
   
-	  public static void main(String[] args) 
-	 	  {
-		    float num = 20.45685f;
-		    System.out.println(num);
+     public static void main(String[] args) 
+      {
+	   float num = 20.45685f;
+           System.out.println(num);
 		 
-		    //float num = 20.5;        //Error
-		 
-		    float num1 = 20.4f;
-        System.out.println(num1);
+           //float num = 20.5;        //Error
+           //System.out.println(num); 
+	     
+           float num1 = 20.4f;
+           System.out.println(num1);
       }
 }
