60e4d0f76f019017ff6294cbf380d88c9bafbedb
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-22 11:58:10 +0530

    LongDataType.java

--- DataTypes/LongDataType.java
+++ DataTypes/LongDataType.java
@@ -4,15 +4,15 @@ public class LongDataType
 {
  //range ---> -2^63 to 2^63-1
 	
-	  public static void main(String[] args)
-	    {
-		    long num = 12312345678l;
-		    System.out.println(num);
+    public static void main(String[] args)
+      {
+        long num = 12312345678l;
+        System.out.println(num);
 		
-		    long num1 = -1245l;
-		    System.out.println(num1);
+        long num1 = -1245l;
+        System.out.println(num1);
 		
-		    long num2 = 7894561234512369877l;
-		    System.out.println(num2);	
-     }
+        long num2 = 7894561234512369877l;
+        System.out.println(num2);	
+      }
 }
