3670cb31639f2bc6dd0c7014652ab96e1fff23b4
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-22 11:21:54 +0530

    CharDataType.java

---
+++ DataTypes/CharDataType.java
@@ -0,0 +1,29 @@
+package com.tns.coreJava;
+
+public class CharDataType 
+{
+  //range ---> 0 to 65535
+	   public static void main(String[] args) 
+	    {
+		    char ch = 65;
+		    System.out.println(ch);
+		  
+		    char ch1 = 128;
+		    System.out.println(ch1);
+		  
+		    char ch2 = 'A';
+		    System.out.println(ch2);
+		  
+		    //char ch = "A";   //Error
+		    //System.out.println(ch);
+		  
+		    //char ch = A;     //Error
+		    // System.out.println(ch);
+		  
+		   //char ch = -a;     //Error
+		   //System.out.println(ch);
+		  
+		   //char ch = '-a';   //Error
+		   // System.out.println(ch);
+	   }
+}
