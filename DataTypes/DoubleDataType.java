d93205967fcfc959fcd4484c8b65f0001e0c4dd6
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-22 11:49:53 +0530

    DoubleDataType.java

---
+++ DataTypes/DoubleDataType.java
@@ -0,0 +1,15 @@
+package com.tns.coreJava;
+
+public class DoubleDataType 
+{
+ //allowed 16 precision no. after the decimal point
+	
+	  public static void main(String[] args) 
+	    {
+		    double num = 52.123456786;
+		    System.out.println(num);
+	  
+		    double num1 = 25.1200;
+		    System.out.println(num1);
+	   }
+}
