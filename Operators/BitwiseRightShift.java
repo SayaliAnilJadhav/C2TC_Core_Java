07f6cca453410021d3f034c5720727936c19530b
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-23 11:14:22 +0530

    BitwiseRightShift.java

---
+++ Operators/BitwiseRightShift.java
@@ -0,0 +1,16 @@
+package com.tns.coreJava;
+
+public class BitwiseRightShift 
+{
+  public static void main(String[] args) 
+    {
+		  int num = 20;
+		  int ans = 0;
+		
+		  ans = num >> 3;
+		  System.out.println(ans);
+		
+		  ans = num >> 2;
+		  System.out.println(ans);
+    }
+}
