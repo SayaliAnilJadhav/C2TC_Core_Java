0a6301cfc6291db408df22f4a5c20e41d5a47d2a
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-23 10:50:47 +0530

    AssingnmentOperator.java

---
+++ Operators/AssingnmentOperator.java
@@ -0,0 +1,13 @@
+package com.tns.coreJava;
+
+public class AssingnmentOperator 
+{
+  public static void main(String[] args) 
+   {
+	  int num = 29;
+	  String name = "M11 corejava";
+	 
+	  System.out.println("Number is assigned= " + num);
+	  System.out.println("Name is assigned= " + name);
+	}
+}
