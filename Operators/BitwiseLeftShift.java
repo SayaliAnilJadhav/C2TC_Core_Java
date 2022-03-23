a7d6451b3382f95c00a5359b2b58a4357fbb0dab
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-23 11:05:13 +0530

    BitwiseLeftShift.java

--- Operators/BitwiseLeftShift.java
+++ Operators/BitwiseLeftShift.java
@@ -4,13 +4,13 @@ public class BitwiseLeftShift
 {
   public static void main(String[] args) 
     {
-	    int num = 15;
-	    int ans = 0;
+	int num = 15;
+	int ans = 0;
 	  
-	    ans = num << 4;
-	    System.out.println(ans);
+	ans = num << 4;
+	System.out.println(ans);
 	  
-	    ans= num << 2;
-	    System.out.println(ans);
- 	 }
+	ans= num << 2;
+	System.out.println(ans);
+    }
 }
