75708c82ef419e9cf01abe7fabee040ef4cbe169
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-23 10:17:29 +0530

    ArithmeticOperator.java

---
+++ Operators/ArithmeticOperator.java
@@ -0,0 +1,26 @@
+package com.tns.coreJava;
+
+public class ArithmeticOperator 
+{
+   public static void main(String[] args)
+    {
+	   int num1 = 10;
+	   int num2 = 5;
+	   int ans = 0;
+	   
+	   ans = num1 + num2;
+	   System.out.println("Addition : " + ans);
+	   
+	   ans = num1 - num2;
+	   System.out.println("Subtraction : " + ans);
+	   
+	   ans = num1 * num2;
+	   System.out.println("Multiplication : " + ans);
+  
+	   ans = num1 / num2;
+	   System.out.println("Division : " + ans);
+	   
+	   ans = num1 % num2;
+	   System.out.println("Modulus : " + ans);
+   }
+}
