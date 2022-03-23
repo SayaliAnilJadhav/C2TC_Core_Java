ac42644ce2fc224cb045fac0cf5253749acc3005
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-23 11:58:43 +0530

    BitwiseOperator.java

--- Operators/BitwiseOperator.java
+++ Operators/BitwiseOperator.java
@@ -4,20 +4,20 @@ public class BitwiseOperator
 {
   public static void main(String[] args) 
     {
-	    int num1 = 5;
-	    int num2 = 9;
-	    int ans = 0;
+	int num1 = 5;
+	int num2 = 9;
+	int ans = 0;
 	  
-	    ans = num1 & num2;                             //bitwise and
-	    System.out.println("Bitwise and= " + ans);
+	ans = num1 & num2;                             //bitwise and
+	System.out.println("Bitwise and= " + ans);
 	  
-	    ans = num1 | num2;                             //bitwise or
-	    System.out.println("Bitwise or= " + ans);
+	ans = num1 | num2;                             //bitwise or
+	System.out.println("Bitwise or= " + ans);
 	  
-	    ans = num1 ^ num2;                            //bitwise xor
-	    System.out.println("Bitwise xor= " + ans);
+	ans = num1 ^ num2;                            //bitwise xor
+	System.out.println("Bitwise xor= " + ans);
 	  
-	    ans = ~num1  ;                                //bitwise not
-	    System.out.println("Bitwise not= " + ans);
-	  }
+	ans = ~num1  ;                                //bitwise not
+	System.out.println("Bitwise not= " + ans);
+    }
 }
