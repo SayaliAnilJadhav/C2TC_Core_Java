2f1a6bf32f2cddfe401d9fb5bee38cba80ebfed1
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-25 23:31:30 +0530

    ArmstrongNumber.java

--- Assignment-2/ArmstrongNumber.java
+++ Assignment-2/ArmstrongNumber.java
@@ -5,23 +5,23 @@ public class ArmstrongNumber
 {
   public static void main(String[] args) 
     {
-	     int num,rem,temp,arm=0;
-	     System.out.println("Enter number= ");
+	 int num,rem,temp,arm=0;
+	 System.out.println("Enter number= ");
 	   
-	     Scanner input = new Scanner(System.in);
-	     num =input.nextInt();
+	 Scanner input = new Scanner(System.in);
+	 num =input.nextInt();
 	   
-	     temp = num;
+	 temp = num;
 	  	
-	     while(num > 0)
-	       {
-		       rem = num % 10;
-		       num = num/10;
-		       arm = arm + rem * rem * rem;
-	       }
-	    if(arm == temp)
-		      System.out.println(temp + " is an Armstrong number");
-	    else
-		      System.out.println(temp + " is not a Armstrong number");
-	 }
+	 while(num > 0)
+	    {
+              rem = num % 10;
+	      num = num/10;
+	      arm = arm + rem * rem * rem;
+	    }
+	 if(arm == temp)
+	      System.out.println(temp + " is an Armstrong number");
+	 else
+              System.out.println(temp + " is not a Armstrong number");
+    }
 }
