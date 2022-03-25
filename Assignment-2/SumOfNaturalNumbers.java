c85432586fba3e026a1547119c4cc268e158af65
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-25 22:32:19 +0530

    SumOfNaturalNumbers.java

---
+++ Assignment-2/SumOfNaturalNumbers.java
@@ -0,0 +1,29 @@
+package com.tns.coreJava;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers 
+{
+  public static void main(String[] args) 
+    {
+	  	int num, i ;
+	  	int sum = 0;
+	  
+	  	Scanner obj = new Scanner(System.in);
+	  
+	  	System.out.println("Sum from= ");
+	  	i = obj.nextInt();
+	  
+	  	System.out.println("Sum up to= ");
+	  	num =obj.nextInt();
+	  
+	  	while(i <= num)
+	  	{
+	  		sum=sum+i;
+	  		i++;
+	  	}
+	  	System.out.println("Sum Of Natural Numbers = " +sum);
+   }
+}
+
+
