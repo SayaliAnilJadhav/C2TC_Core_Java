5a8eadee07da4b1407be39661176edabe8d2e59c
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-26 01:02:02 +0530

    ReversePyramid_StarPattern.java

--- Assignment-2/ReversePyramid_StarPattern.java
+++ Assignment-2/ReversePyramid_StarPattern.java
@@ -7,16 +7,16 @@ public class ReversePyramid_StarPattern
 	    int row = 5;
 	  
 	    for(int i = 0; i <= row - 1; i++)
-	  	  {
-		      for(int j = 0; j <= i; j++)
-		       {
-			      System.out.print(" ");
-		       }
-		     for(int k = 0;k <= row - 1 - i; k++)
-		      {
-			      System.out.print("*"+" "); 
-		      }
-		     System.out.println();
-	  	 }
-   }
+	       {
+		  for(int j = 0; j <= i; j++)
+		     {
+		       System.out.print(" ");
+		     }
+		  for(int k = 0;k <= row - 1 - i; k++)
+		     {
+		       System.out.print("*"+" "); 
+		     }
+		  System.out.println();
+	      }
+         }
 }
