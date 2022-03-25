8ef83ff15a010884dc9f79bb38790ed8a1e167f3
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-25 22:35:08 +0530

    SumOfNaturalNumbers.java

--- Assignment-2/SumOfNaturalNumbers.java
+++ Assignment-2/SumOfNaturalNumbers.java
@@ -6,24 +6,24 @@ public class SumOfNaturalNumbers
 {
   public static void main(String[] args) 
     {
-	  	int num, i ;
-	  	int sum = 0;
+	  int num, i ;
+	  int sum = 0;
 	  
-	  	Scanner obj = new Scanner(System.in);
+	  Scanner obj = new Scanner(System.in);
 	  
-	  	System.out.println("Sum from= ");
-	  	i = obj.nextInt();
+	  System.out.println("Sum from= ");
+	  i = obj.nextInt();
 	  
-	  	System.out.println("Sum up to= ");
-	  	num =obj.nextInt();
+	  System.out.println("Sum up to= ");
+	  num =obj.nextInt();
 	  
-	  	while(i <= num)
-	  	{
-	  		sum=sum+i;
-	  		i++;
-	  	}
-	  	System.out.println("Sum Of Natural Numbers = " +sum);
-   }
+	  while(i <= num)
+	    {
+	  	sum=sum+i;
+	  	i++;
+	    }
+	 System.out.println("Sum Of Natural Numbers = " +sum);
+    }
 }
 
 
