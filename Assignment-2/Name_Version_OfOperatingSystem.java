cb7bcbff5636462312ffbd5a7d4861e2740d1155
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-26 10:10:31 +0530

    Name_Version_OfOperatingSystem.java

---
+++ Assignment-2/Name_Version_OfOperatingSystem.java
@@ -0,0 +1,13 @@
+package com.tns.coreJava;
+
+public class Name_Version_OfOperatingSystem 
+{
+  public static void main(String[] args)
+  	{
+	    System.out.println("Operating System Name =");
+	    System.out.println(System.getProperty("os.name"));
+	  
+	    System.out.println("Operating System Version =");
+	    System.out.println(System.getProperty("os.version"));
+	  }
+}
