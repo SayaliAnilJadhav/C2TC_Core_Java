23521d34d3bbe554e25141b8e7b214cf3afbd4ad
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-26 11:05:52 +0530

    LeftTriangle_StarPattern.java

--- Assignment-2/LeftTriangle_StarPattern.java
+++ Assignment-2/LeftTriangle_StarPattern.java
@@ -4,8 +4,8 @@ public class LeftTriangle_StarPattern
 {
    public static void main(String[] args) 
      {
-	   int i, j;
-	   int row=5;
+       int i, j;
+       int row=5;
 	   
        for (i = 0; i < row; i++) 
        {
