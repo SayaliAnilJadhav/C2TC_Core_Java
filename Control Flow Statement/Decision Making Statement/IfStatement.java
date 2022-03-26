0decfaf214f8a766226671cd7441d09343fbbc23
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-03-26 11:28:00 +0530

    IfStatement.java

---
+++ Control Flow Statement/Decision Making Statement/IfStatement.java
@@ -0,0 +1,15 @@
+package com.tns.coreJava;
+
+public class IfStatement 
+{
+  public static void main(String[] args) 
+    {
+	  	int num = 5;
+	  	
+	  	if(num < 0)
+	  	{
+	  		System.out.println(num + " is a negative number");
+	  	}
+	  	System.out.println(" Satement ouside if block");
+     }
+}
