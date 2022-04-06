ee5c84e56408b0d93f0039750920580a7b548e8d
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-06 12:14:50 +0530

    ShopAcc.java

---
+++ Go Shopping/Framework/ShopAcc.java
@@ -0,0 +1,59 @@
+package com.tns.framework;
+
+ public abstract class ShopAcc 
+ {
+	  //Here  declaring account number, account name, charges for shopping
+	  private int accNo;
+	  private String accNm;
+	  private float charges;
+	
+	  //Here getting account number
+	   public int getAccNo() 
+	  { 
+		  return accNo;
+	  }
+
+	  //Here getting account name
+	  public String getAccNm() 
+	  {
+		  return accNm;
+	  }
+
+    //Here setting account name
+    public void setAccNm(String accNm)
+     {
+		   this.accNm = accNm;
+	   }
+
+    //Here getting charges for shopping
+    public float getCharges() 
+    {
+		  return charges;
+	  }
+
+   //Here defining the constructor for shopping account class
+   public ShopAcc(int accNo, String accNm, float charges) 
+    {
+		  this.accNo = accNo;
+		  this.accNm = accNm;
+		  this.charges = charges;
+	  }
+ 
+    //Here defining abstract bookProduct
+	  public void bookProduct(float pro)
+	  {
+      
+	  }
+	
+	  //Here defining item method  to display the item
+	  public void item(float value)
+	  {
+		
+	  }
+	
+    @Override
+	  public String toString() 
+	  {
+		  return accNm;
+	  }
+}
