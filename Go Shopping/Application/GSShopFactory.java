4c74d4c44d1bfa0c56599c695f4939af0c80e6fa
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-06 12:44:02 +0530

    GSShopFactory.java

--- Go Shopping/Application/GSShopFactory.java
+++ Go Shopping/Application/GSShopFactory.java
@@ -7,20 +7,20 @@ import com.tns.framework.ShopFactory;
 public class GSShopFactory extends ShopFactory 
 {
   @Override
-	public PrimeAcc getNewPrimeAccount(int accNo, String accNm, float charges, boolean isPrime) 
-	 {
-		GSPrimeAcc GSPrime = new GSPrimeAcc(accNo, accNm, charges,  isPrime);
-		
-		System.out.println(GSPrime.toString());
-		return GSPrime;
-	 }
+  public PrimeAcc getNewPrimeAccount(int accNo, String accNm, float charges, boolean isPrime) 
+    {
+	GSPrimeAcc GSPrime = new GSPrimeAcc(accNo, accNm, charges,  isPrime);
+	
+	System.out.println(GSPrime.toString());
+	return GSPrime;
+    }
 
-	@Override
-	public NormalAcc getNewNormalAccount(int accNo, String accNm, float charges, float deliveryCharges) 
-	 {
-		GSNormalAcc GSNormal = new GSNormalAcc(accNo, accNm, charges, deliveryCharges);
+   @Override
+   public NormalAcc getNewNormalAccount(int accNo, String accNm, float charges, float deliveryCharges) 
+     {
+	  GSNormalAcc GSNormal = new GSNormalAcc(accNo, accNm, charges, deliveryCharges);
 
-		System.out.println(GSNormal.toString());
-		return GSNormal;
-	 }
+	  System.out.println(GSNormal.toString());
+	  return GSNormal;
+     }
 }
