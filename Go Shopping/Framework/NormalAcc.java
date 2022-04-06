7db84f47e670e8772978bf34a94e0418c349b92b
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-06 12:25:28 +0530

    NormalAcc.java

---
+++ Go Shopping/Framework/NormalAcc.java
@@ -0,0 +1,35 @@
+package com.tns.framework;
+
+public abstract class NormalAcc extends ShopAcc 
+{
+  //Here declaring deliveryCharges who is not belong to prime user
+  private final float deliveryCharges=49;
+  
+  float charges;
+  
+ //Here getting delivery charges 
+ public float getDeliveryCharges() 
+  {
+	  return deliveryCharges;
+  }
+ 
+ //Here defining constructor for the NormalAcc:
+ public NormalAcc(int accNo,String accNm,float charges)
+ {
+	 super(accNo,accNm,charges);
+ }
+ 
+ @Override
+ public void bookProduct(float b) 
+ {
+		System.out.println();
+ }
+
+  @Override
+  public String toString() 
+	{
+	  return "Dear Normal user, Your Product charges are:" + charges + " with Delivery Charges is : " + deliveryCharges ;
+  }
+}
+
+
