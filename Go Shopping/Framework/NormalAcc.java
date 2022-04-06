00936e4450b547783c779926a00578853e1562f3
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-06 12:28:19 +0530

    NormalAcc.java

--- Go Shopping/Framework/NormalAcc.java
+++ Go Shopping/Framework/NormalAcc.java
@@ -10,26 +10,26 @@ public abstract class NormalAcc extends ShopAcc
  //Here getting delivery charges 
  public float getDeliveryCharges() 
   {
-	  return deliveryCharges;
+      return deliveryCharges;
   }
  
  //Here defining constructor for the NormalAcc:
  public NormalAcc(int accNo,String accNm,float charges)
  {
-	 super(accNo,accNm,charges);
+     super(accNo,accNm,charges);
  }
  
  @Override
  public void bookProduct(float b) 
  {
-		System.out.println();
+     System.out.println();
  }
 
   @Override
   public String toString() 
-	{
-	  return "Dear Normal user, Your Product charges are:" + charges + " with Delivery Charges is : " + deliveryCharges ;
-  }
+   {
+	return "Dear Normal user, Your Product charges are: " + charges + " with Delivery Charges is : " + deliveryCharges ;
+   }
 }
 
 
