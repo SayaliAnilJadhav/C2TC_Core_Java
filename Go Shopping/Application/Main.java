83ec6165b2ab10c6e2b4f07c71bc5719546e9b03
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-06 12:45:36 +0530

    Main.java

--- Go Shopping/Application/Main.java
+++ Go Shopping/Application/Main.java
@@ -3,9 +3,9 @@ package com.tns.application;
 import com.tns.framework.NormalAcc;
 import com.tns.framework.PrimeAcc;
 
-public class Main {
-
-	public static void main(String[] args) 
+public class Main 
+{
+     public static void main(String[] args) 
 	{
 		PrimeAcc prime = new GSPrimeAcc(1, "Sayali", 6000, true);
 		NormalAcc normal = new GSNormalAcc(2, "Shravani", 12000, 49);
