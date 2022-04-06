615e9d863c9275216d891cdb47a9567974726efb
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-06 12:39:57 +0530

    GSNormalAcc.java

--- Go Shopping/Application/GSNormalAcc.java
+++ Go Shopping/Application/GSNormalAcc.java
@@ -4,24 +4,24 @@ import com.tns.framework.NormalAcc;
 
 public class GSNormalAcc extends NormalAcc 
 {
-	//Here defining the constructor of GsNormalAcc class
-	public GSNormalAcc(int accNo, String accNm, float charges , float deliveryCharges)
-	 {
-		 super(accNo,accNm,charges);
-	 }
+  //Here defining the constructor of GsNormalAcc class
+  public GSNormalAcc(int accNo, String accNm, float charges , float deliveryCharges)
+    {
+       super(accNo,accNm,charges);
+    }
 	    
- //Here defiining the method of bookProduct
- @Override
- public void bookProduct(float d) 
-	{
-		 // TODO Auto-generated method stub
-		 super.bookProduct(d);
-	}
+  //Here defiining the method of bookProduct
+  @Override
+  public void bookProduct(float d) 
+    {
+	// TODO Auto-generated method stub
+	super.bookProduct(d);
+    }
 	 
- @Override
- public String toString() 
-	{
-			// TODO Auto-generated method stub
-			return super.toString();
-	}
+  @Override
+  public String toString() 
+    {
+        // TODO Auto-generated method stub
+	return super.toString();
+    }
 }
