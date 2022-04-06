bad376a524abe47f74c5513b0df238c5730dd8c0
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-06 12:35:41 +0530

    GSPrimeAcc.java

--- Go Shopping/Application/GSPrimeAcc.java
+++ Go Shopping/Application/GSPrimeAcc.java
@@ -5,32 +5,31 @@ import com.tns.framework.PrimeAcc;
 public class GSPrimeAcc extends PrimeAcc 
 {
   //Here declaring the Charges variable 
-	private static final float Charges=0;
+  private static final float Charges=0;
 	
-	//Here getting the method of Charges
-	public float getCharges()
-	{
-		return Charges;
-	}
+ //Here getting the method of Charges
+ public float getCharges()
+   {
+	return Charges;
+   }
 	
-	//Defining the Constructor of GSPrimeAcc
-	public GSPrimeAcc(int accNo,String accNm, float charges, boolean isPrime)
-	{
-		super(accNo,accNm,charges,isPrime);
+ //Defining the Constructor of GSPrimeAcc
+ public GSPrimeAcc(int accNo,String accNm, float charges, boolean isPrime)
+   {
+	super(accNo,accNm,charges,isPrime);
+   }
 	
-	}
-	
-	//Here defining the method of bookProduct
-	@Override
-	public void bookProduct(float c) 
+ //Here defining the method of bookProduct
+ @Override
+ public void bookProduct(float c) 
   {
-		super.bookProduct(c);
-	}
+	super.bookProduct(c);
+  }
 
-	//Here defining the toString Method
-	@Override
-	public String toString() 
+ //Here defining the toString Method
+ @Override
+ public String toString() 
   {
-	  return super.toString();
-	}
+        return super.toString();
+  }
 }
