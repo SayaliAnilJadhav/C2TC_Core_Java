3364fd6665dc99e69588eef85eb5e3ce101e7651
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-06 12:18:37 +0530

    ShopAcc.java

--- Go Shopping/Framework/ShopAcc.java
+++ Go Shopping/Framework/ShopAcc.java
@@ -10,50 +10,50 @@ package com.tns.framework;
 	  //Here getting account number
 	   public int getAccNo() 
 	  { 
-		  return accNo;
+            return accNo;
 	  }
 
 	  //Here getting account name
 	  public String getAccNm() 
 	  {
-		  return accNm;
+	    return accNm;
 	  }
 
-    //Here setting account name
-    public void setAccNm(String accNm)
-     {
-		   this.accNm = accNm;
-	   }
+         //Here setting account name
+         public void setAccNm(String accNm)
+         {
+	   this.accNm = accNm;
+	 }
 
-    //Here getting charges for shopping
-    public float getCharges() 
-    {
-		  return charges;
-	  }
+       //Here getting charges for shopping
+       public float getCharges() 
+        {
+	  return charges;
+	}
 
-   //Here defining the constructor for shopping account class
-   public ShopAcc(int accNo, String accNm, float charges) 
-    {
-		  this.accNo = accNo;
-		  this.accNm = accNm;
-		  this.charges = charges;
-	  }
+       //Here defining the constructor for shopping account class
+       public ShopAcc(int accNo, String accNm, float charges) 
+        {
+          this.accNo = accNo;
+          this.accNm = accNm;
+          this.charges = charges;
+	}
  
-    //Here defining abstract bookProduct
-	  public void bookProduct(float pro)
-	  {
+       //Here defining abstract bookProduct
+       public void bookProduct(float pro)
+	 {
       
-	  }
+	 }
 	
-	  //Here defining item method  to display the item
-	  public void item(float value)
-	  {
+	//Here defining item method  to display the item
+	public void item(float value)
+	 {
 		
-	  }
+	 }
 	
-    @Override
-	  public String toString() 
+        @Override
+	public String toString() 
 	  {
-		  return accNm;
+            return accNm;
 	  }
 }
