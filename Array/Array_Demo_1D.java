fc65fd77b3106d4ee665a395ac08f9bdf10c06b4
SayaliAnilJadhav <94784544+SayaliAnilJadhav@users.noreply.github.com>
2022-04-26 07:37:34 +0530

    Array Program

---
+++ Array/Array_Demo_1D.java
@@ -0,0 +1,15 @@
+package com.tns.coreJava;
+
+public class Array_Demo_1D {
+
+	public static void main(String[] args) {
+		
+		int[] arr= {24,40,68,34,10,};
+		System.out.println("First Value  :"+arr[0]);
+		System.out.println("Second Value :"+arr[1]);
+		System.out.println("Third Value  :"+arr[2]);
+		System.out.println("Fourth Value :"+arr[3]);
+		System.out.println("Fifth Value  :"+arr[4]);
+    }
+
+}
